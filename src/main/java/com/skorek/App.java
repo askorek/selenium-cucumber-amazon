7cc8a05364acbf89c767a309b036db5b65fb6d69
Adam Skorek <adam.skorek@nokia.com>
2017-10-21 00:16:29 +0200

    create project

---
+++ src/main/java/com/skorek/App.java
@@ -0,0 +1,13 @@
+package com.skorek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
